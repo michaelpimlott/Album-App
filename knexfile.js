c09d18a59facc634170fe3902f99f7dab40a8e52
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-30 15:48:16 -0700

    add psql database

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/album-demo'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  }
+
+};
--- routes/albums.js
+++ routes/albums.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-router.get('/albums', function(req, res, next){
-  res.render('albums/index');
+function Albums() {
+  return knex('albums');
+}
+
+router.get('/albums', function(req, res, next) {
+  Albums().select().then(function(records) {
+    res.render('albums/index', {allAlbums: records});
+  });
 });
 
-router.get('/albums/new', function(req, res, next){
+router.get('/albums/new', function(req, res, next) {
   res.render('albums/new');
 });
 
+router.post('/albums', function(req, res, next) {
+  Albums().insert({
+    artist: req.body.artist_name,
+    name: req.body.album_name,
+    genre: req.body.genre,
+    stars: req.body.stars,
+    explicit: req.body.explicit
+  }).then(function() {
+      res.redirect('/albums');
+  });
+});
 module.exports = router;
