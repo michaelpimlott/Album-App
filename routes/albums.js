92a97ea97d5b3f43a6f70aefe10833aafefbe9bc
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-30 17:24:51 -0700

    made everything pink

--- routes/albums.js
+++ routes/albums.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
+var methodOverride = require('method-override');
 
+
+var genreOptions = ['Rubbish','Funxploitation', 'Clearance', 'Civil War Jams', 'Big Banjo', 'Soundtrack', 'Field Recordings', 'Jazz'];
+var ratingOptions = [1,2,3,4,5];
+
+var knex = require('../db/knex');
 function Albums() {
   return knex('albums');
 }
+
+router.use(methodOverride('_method'));
 
 router.get('/albums', function(req, res, next) {
   Albums().select().then(function(records) {
@@ -12,14 +19,14 @@ router.get('/albums', function(req, res, next) {
   });
 });
 
+router.get('/albums/new', function(req, res, next) {
+  res.render('albums/new');
+});
+
 router.get('/albums/:id', function(req, res, next) {
   Albums().where({id: req.params.id}).first().then(function(record){
     res.render('albums/show', {theAlbum: record});
   });
-});
-
-router.get('/albums/new', function(req, res, next) {
-  res.render('albums/new');
 });
 
 router.post('/albums', function(req, res, next) {
@@ -37,6 +44,12 @@ router.post('/albums', function(req, res, next) {
 router.get('/albums/:id/edit', function(req, res, next){
   Albums().where({ id: req.params.id}).first().then(function(record){
     res.render('albums/edit', {theAlbum: record, options: genreOptions});
+  });
+});
+
+router.put('/albums/:id', function(req, res, next) {
+  Albums().where({ id: req.params.id }).update({ artist: req.body.artist_name, name: req.body.album_name, genre: req.body.genre, stars: req.body.stars, explicit: req.body.explicit }).then(function (record) {
+    res.redirect('/albums/' + req.params.id);
   });
 });
 
